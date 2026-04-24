b33074340b6b8a331681c008e284e976724bbaa6
KATYAYNI20 <katyayniksha01@gmail.com>
2026-04-24 08:23:40 +0530

    Input_output

---
+++ Basics/General_Logics/inp_out.py
@@ -0,0 +1,11 @@
+class Solution:
+    def printNumber(self):
+        print(int(input("Enter the number: ")))
+
+def main():
+    x = Solution()
+    x.printNumber()
+
+
+if __name__ == "__main__":
+    main()
